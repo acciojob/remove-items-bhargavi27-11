b311132e632e65878d075771d4a0db5349522194
sesetty bhargavi <sesettybhargavi@gmail.com>
2026-04-01 10:18:55 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
 //your JS code here. If required.
+const select = document.getElementsById("colorselect");
+const button = document.querySelector('input[type="button"]');
+
+button.addEventListener("click",function(){
+	const selectedIndex = select.selectedIndex;
+
+	if(selectedIndex !== -1){
+		select.remove(selectedIndex);
+	}
+});
